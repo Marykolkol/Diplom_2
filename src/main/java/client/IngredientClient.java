53cd0abfff44eaba1a8f8ad2c60dfe3d97345760
Mariya <kolomina.mariya@wb.ru>
2025-03-26 23:21:30 +0300

    done

---
+++ src/main/java/client/IngredientClient.java
@@ -0,0 +1,44 @@
+package client;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static config.StellarBurgerConfig.STELLAR_BASE_URI;
+import static io.restassured.RestAssured.given;
+
+public class IngredientClient {
+    private static final Random RANDOM = new Random();
+
+    @Step("Генерация списка ингредиентов")
+    public List<String> generateIngredientsList(List<Map<String,String>> ingredients) {
+        List<String> ingredientsForOrder = new ArrayList<>();
+        int count = RANDOM.nextInt(ingredients.size()-1)+1;
+        for (int i = 0; i < count; i++) {
+            ingredientsForOrder.add(ingredients.get(i).get("_id"));
+        }
+        return ingredientsForOrder;
+    }
+
+    @Step("Получение списка ингредиентов")
+    public ValidatableResponse getListOfIngredients(){
+        return
+                given()
+                        .log()
+                        .all()
+                        .baseUri(STELLAR_BASE_URI)
+                        .header("Content-type", "application/json")
+                        .when()
+                        .get("/api/ingredients")
+                        .then()
+                        .log()
+                        .all();
+    }
+
+}
